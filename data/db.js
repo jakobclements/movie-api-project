8b2305536bedf72614db4f2afc301f82d87cdd91
jakobclements <45043451+jakobclements@users.noreply.github.com>
2025-05-25 20:42:47 -0400

    Implement SQLite database, convert endpoints from arrays to SQLite statements

---
+++ data/db.js
@@ -0,0 +1,36 @@
+const Database = require('better-sqlite3');
+const db = new Database('library.db', { verbose: console.log });
+
+// Create movies table if not exists
+db.prepare(`
+    CREATE TABLE IF NOT EXISTS movies (
+        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+        title TEXT,
+        year TEXT
+    )
+`).run();
+
+// Populate movies table if empty
+const rows = db.prepare('SELECT COUNT(*) AS rows FROM movies').get().rows;
+if (rows === 0) {
+    console.log('0 rows found, adding data...')
+
+    // Starting data
+    const movies = [
+        {id: 1, title: 'The Matrix', year: '1999'},
+        {id: 2, title: 'Inception', year: '2010'}
+    ];
+
+    // Insert starting data
+    const insert = db.prepare(`INSERT INTO movies (title, year) VALUES (?, ?)`);
+    movies.forEach(movie => {
+        insert.run(movie.title, movie.year);
+    });
+
+    console.log('Database initialized');
+}
+
+// Show data for debugging
+console.log(db.prepare('SELECT * FROM movies').all());
+
+module.exports = db;
--- routes/api/library.js
+++ routes/api/library.js
@@ -1,53 +1,47 @@
 const express = require('express');
 const router = express.Router();
-
-// Mock data for testing
-let movies = [
-    {id: 1, title: 'The Matrix', year: '1999'},
-    {id: 2, title: 'Inception', year: '2010'}
-];
+const db = require('../../data/db');
 
 // GET: list all movies
 router.get('/', (req, res) => {
+    const movies = db.prepare(`SELECT * FROM movies`).all();
     res.json(movies);
 });
 
 // GET: details about a movie
 router.get('/:id', (req, res) => {
     const id = parseInt(req.params.id);
-    const movie = movies.find(movie => movie.id === id);
+    const movie = db.prepare('SELECT * FROM movies WHERE id = ?').get(id);
     if (!movie) {return res.json('Movie not found')};
     res.json(movie);
 });
 
 // POST: add a movie
 router.post('/add', (req, res) => {
-    const maxId = movies.length === 0 ? 0 : Math.max(...movies.map(movie => movie.id));
     const movie = {
-        id: maxId + 1,
         title: req.body.title,
         year: req.body.year
     };
-    movies.push(movie);
+    db.prepare(`INSERT INTO movies (title, year) VALUES (?, ?)`).run(movie.title, movie.year);
     res.json('Added movie');
 });
 
 // PUT: edit movie info
 router.put('/edit/:id', (req, res) => {
     const id = parseInt(req.params.id);
-    const movie = movies.find(movie => movie.id === id);
+    const movie = db.prepare('SELECT * FROM movies WHERE id = ?').get(id);
     if (!movie) {return res.json('Movie not found');}
-    if (req.body.title) {movie.title = req.body.title;}
-    if (req.body.year) {movie.year = req.body.year;}
+    if (req.body.title) {db.prepare('UPDATE movies SET title = ? WHERE id = ?').run(req.body.title, id);}
+    if (req.body.year) {db.prepare('UPDATE movies SET year = ? WHERE id = ?').run(req.body.year, id);}
     res.json('Edited movie');
 });
 
 // DELETE: remove movie
 router.delete('/delete/:id', (req, res) => {
     const id = parseInt(req.params.id);
-    const index = movies.findIndex(movie => movie.id === id);
-    if (index === -1) {return res.json('Movie not found');}
-    movies.splice(index, 1);
+    const movie = db.prepare('SELECT * FROM movies WHERE id = ?').get(id);
+    if (!movie) {return res.json('Movie not found');}
+    db.prepare('DELETE FROM movies WHERE id = ?').run(id);
     res.json('Deleted movie');
 });
 
