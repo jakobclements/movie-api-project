8a11a7db94405291c01415e8b57a98282fddcf72
jakobclements <45043451+jakobclements@users.noreply.github.com>
2025-05-27 08:58:17 -0400

    Fix search.js formatting for readability

--- routes/api/search.js
+++ routes/api/search.js
@@ -7,18 +7,24 @@ const TMDB_API_KEY = process.env.TMDB_API_KEY;
 
 // Search TMDB
 router.get('/', async (req, res) => {
+
     // Get query param
     const query = req.query.q;
+
     // If no query, exit
     if (!query) {return res.json('No results');}
+
     try {
         // Call TMDB API with search query
         const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${TMDB_API_KEY}&query=${query}`);
+
         // If response not ok, exit
         if (!response.ok) {return res.json("Error fetching from TMDB");}
+
         // Show results
         const results = await response.json();
         res.json(results.results);
+        
     // Catch errors
     } catch (error) {res.json('Error');}
 });
